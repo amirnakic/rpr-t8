ad86c26f3695e1ee9c31f14f7b3274e3d475d06f
amirnakic <anakic1@etf.unsa.ba>
2018-12-11 17:49:24 +0100

    Implementirane su funkcionalnosti tražene u sklopu 1. zadatka, a to su progress-bar i onemogućavanje dugmadi onako kako je navedeno u postavci zadatka.

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 
 import java.io.File;
@@ -22,22 +23,32 @@ public class Controller {
     public TextField searchField;
     public Button searchBtn;
     public ListView list;
-    public List<String> result;
+    public List<String> result1;
+    public Button stopButton;
+    public ProgressBar progressBar;
+    public boolean prekidanje = false;
+
+    private Thread thread1, thread2;
 
     public Controller() {
         text = new SimpleStringProperty("");
         lista = new SimpleListProperty<>();
-        result = Collections.synchronizedList(new ArrayList<>());
+        result1 = Collections.synchronizedList(new ArrayList<>());
+        progressBar = new ProgressBar(0);
     }
 
     @FXML
     public void initialize() {
         searchField.textProperty().bindBidirectional(text);
         list.itemsProperty().bindBidirectional(lista);
-        lista.set(FXCollections.observableArrayList(result));
+        lista.set(FXCollections.observableArrayList(result1));
+        stopButton.setDisable(true);
+        prekidanje = false;
+        progressBar.setProgress(0);
     }
 
     public void dajFajloveKojiSePodudaraju(File f) {
+        if (prekidanje) return;
         try {
             File[] files = f.listFiles();
             if (files == null) return;
@@ -47,7 +58,9 @@ public class Controller {
                 } else {
                     if (file.getCanonicalPath().toLowerCase().contains(searchField.getText().toLowerCase())) {
                         String result = file.getCanonicalPath();
-                        Platform.runLater(() -> list.getItems().add(result));
+                        Platform.runLater(() -> {
+                            list.getItems().add(result);
+                        });
                     }
                 }
             }
@@ -57,8 +70,39 @@ public class Controller {
     }
 
     public void clickOnSearchBtn(ActionEvent actionEvent) {
-        Runnable r = () -> dajFajloveKojiSePodudaraju(new File(System.getProperty("user.home")));
-        Thread thread = new Thread(r);
-        thread.start();
+        initialize();
+        Runnable r1 = () -> {
+            searchBtn.setDisable(true);
+            searchField.setDisable(true);
+            stopButton.setDisable(false);
+            dajFajloveKojiSePodudaraju(new File(System.getProperty("user.home")));
+        };
+        Runnable r2 = () -> {
+            for (int i = 1; i <= 1000; i++) {
+                if (prekidanje) {
+                    progressBar.setProgress(1000);
+                    break;
+                }
+                progressBar.setProgress(i / 1000.0);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread1 = new Thread(r1);
+        thread2 = new Thread(r2);
+        thread1.start();
+        thread2.start();
+    }
+
+    public void clickOnStopButton(ActionEvent actionEvent) {
+        if (thread1!= null || thread2!=null) {
+            prekidanje = true;
+            searchBtn.setDisable(false);
+            searchField.setDisable(false);
+            stopButton.setDisable(true);
+        }
     }
 }
