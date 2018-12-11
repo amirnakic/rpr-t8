064add77c2dd3fa9a77c281c34b8dd9850ddedea
amirnakic <anakic1@etf.unsa.ba>
2018-12-11 02:39:34 +0100

    Dodana je metoda dajFajloveKojiSePodudaraju kojom dobijamo listu podudarajućih fajlova na osnovu unosa teksta u sklopu pretrage.

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -1,25 +1,59 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Controller {
+    public SimpleStringProperty text;
+    public SimpleListProperty<String> lista;
     public TextField searchField;
     public Button searchBtn;
     public ListView list;
+    public List<String> result;
 
-    private SimpleStringProperty text;
     public Controller() {
         text = new SimpleStringProperty("");
+        lista = new SimpleListProperty<>();
+        result = Collections.synchronizedList(new ArrayList<>());
     }
 
     @FXML
     public void initialize() {
         searchField.textProperty().bindBidirectional(text);
+        list.itemsProperty().bindBidirectional(lista);
+        lista.set(FXCollections.observableArrayList(result));
+    }
+
+    public void dajFajloveKojiSePodudaraju(File f) {
+        try {
+            File[] files = f.listFiles();
+            if (files == null) return;
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    dajFajloveKojiSePodudaraju(file);
+                } else {
+                    if (file.getCanonicalPath().toLowerCase().contains(searchField.getText().toLowerCase())) {
+                        String result = file.getCanonicalPath();
+                        Platform.runLater(() -> list.getItems().add(result));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clickOnSearchBtn(ActionEvent actionEvent) {
