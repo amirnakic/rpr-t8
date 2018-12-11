8b82d1c2acba4d1b3b01f2f30992ced7e7cf8f23
amirnakic <anakic1@etf.unsa.ba>
2018-12-11 01:19:03 +0100

    Dodani su atributi i metode karakterističnih polja, te je uspostavljen bidirectionalbinding.

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField searchField;
+    public Button searchBtn;
+    public ListView list;
+
+    private SimpleStringProperty text;
+    public Controller() {
+        text = new SimpleStringProperty("");
+    }
+
+    @FXML
+    public void initialize() {
+        searchField.textProperty().bindBidirectional(text);
+    }
+
+    public void clickOnSearchBtn(ActionEvent actionEvent) {
+    }
 }
